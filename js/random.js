f575df5b3ef9c5b9546ea42943cae34f2d03eb5c
JJ299 <62095762+JJ299@users.noreply.github.com>
2020-07-19 18:19:47 -0400

    Finalized JS

--- js/random.js
+++ js/random.js
@@ -1,4 +1,22 @@
-Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+const one = prompt("Please enter the first number.");
+const two = prompt("Please enter the second number.");
+const ones = +one;
+const twos =+two;
+let randomizer;
 
+const random = (numberone, numbertwo) => {
+	if (numberone>numbertwo){
+		randomizer = Math.floor(Math.random()*(numbertwo-numberone+1)) + numberone;
+		return randomizer;
+	} else {
+		randomizer = Math.floor(Math.random()*(numberone-numbertwo+1)) + numbertwo;
+		return randomizer;
+	}
+	
+}
 
+random(ones,twos);
 
+const final = `<p>Your random number is: ${randomizer} </p>`;
+
+document.querySelector(`main`).innerHTML = final;

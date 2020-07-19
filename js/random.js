f1b6f663a488f19695d8269510c3a693692927f0
JJ299 <62095762+JJ299@users.noreply.github.com>
2020-07-19 19:19:07 -0400

    Learned isNaN. Added JS feature where if the user doesn't input a number it shows that an error message

--- js/random.js
+++ js/random.js
@@ -5,18 +5,26 @@ const twos =+two;
 let randomizer;
 
 const random = (numberone, numbertwo) => {
-	if (numberone>numbertwo){
-		randomizer = Math.floor(Math.random()*(numbertwo-numberone+1)) + numberone;
-		return randomizer;
-	} else {
-		randomizer = Math.floor(Math.random()*(numberone-numbertwo+1)) + numbertwo;
+
+	if( isNaN(numberone) || isNaN(numbertwo)) {
+		randomizer = "Both arguments must be a number";
 		return randomizer;
 	}
-	
+
+	if (numberone<numbertwo){
+		randomizer = Math.floor(Math.random()*(numbertwo-numberone+1)) + numberone;
+		return randomizer;
+	} else if(numberone>numbertwo) {
+		randomizer = Math.floor(Math.random()*(numberone-numbertwo+1)) + numbertwo;
+		return randomizer;
+	} 
 }
 
 random(ones,twos);
 
 const final = `<p>Your random number is: ${randomizer} </p>`;
 
-document.querySelector(`main`).innerHTML = final;
+document.querySelector(`main`).innerHTML = final; 
+
+
+
